e88851a7621e6f3889b4156e3d0893aaea9ea99b
usuario <usuario@10.22.148.63>
2017-11-10 09:52:09 +0000

    Kata5P1 v2

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,14 +1,20 @@
 package kata5p1;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 public class Kata5P1 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
         Class.forName("org.sqlite.JDBC");
         Connection cn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\usuario\\Desktop\\SQLiteDatabaseBrowserPortable\\Data\\Kata5.db");
         
@@ -17,6 +23,29 @@ public class Kata5P1 {
         
         while (rs.next()) {
             System.out.println("NAME = " + rs.getString("name"));
+        }
+        
+        String query = "CREATE TABLE IF NOT EXISTS mail (\n"
+                + "     Id INTEGER PRIMARY KEY AUTOINCREMENT, \n"
+                + "     Mail TEXT NOT NULL\n"
+                + ");";
+        System.out.println(query);
+        statement.execute(query);
+        
+        String nameMails = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata5P1\\src\\kata5p1\\emails.txt";
+        ArrayList<String> mails = new ArrayList<>();
+        BufferedReader filein = new BufferedReader(new FileReader(new File(nameMails)));
+        
+        String mail;
+        while((mail = filein.readLine()) != null) {
+            if(!mail.contains("@")) continue;
+            mails.add(mail);
+        }
+        
+        for (String m : mails) {
+            query = "INSERT INTO mail (mail) VALUES ('" + m + "');";
+            System.out.println(query);
+            statement.executeUpdate(query);
         }
     }
 }
