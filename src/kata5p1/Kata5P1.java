7463e16d16dfd5f802f4f041eb827fe073c1c0d4
usuario <usuario@10.22.148.63>
2017-11-24 10:18:06 +0000

    version 3 Kata5p1

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -1,51 +1,28 @@
 package kata5p1;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.sql.Statement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
+import java.util.Calendar;
 
 public class Kata5P1 {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, FileNotFoundException, IOException {
-        Class.forName("org.sqlite.JDBC");
-        Connection cn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\usuario\\Desktop\\SQLiteDatabaseBrowserPortable\\Data\\Kata5.db");
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+        Connection cn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:orcl","system","orcl");
         
         Statement statement = cn.createStatement();
-        ResultSet rs = statement.executeQuery("SELECT name FROM PEOPLE");
+        
+        String query = "SELECT * FROM HISTORICO_CAMBIOS WHERE DIVISA_DESDE = 'EUR' AND DIVISA_A = 'USD'";
+        ResultSet rs = statement.executeQuery(query);
         
         while (rs.next()) {
-            System.out.println("NAME = " + rs.getString("name"));
-        }
-        
-        String query = "CREATE TABLE IF NOT EXISTS mail (\n"
-                + "     Id INTEGER PRIMARY KEY AUTOINCREMENT, \n"
-                + "     Mail TEXT NOT NULL\n"
-                + ");";
-        System.out.println(query);
-        statement.execute(query);
-        
-        String nameMails = "C:\\Users\\usuario\\Documents\\NetBeansProjects\\Kata5P1\\src\\kata5p1\\emails.txt";
-        ArrayList<String> mails = new ArrayList<>();
-        BufferedReader filein = new BufferedReader(new FileReader(new File(nameMails)));
-        
-        String mail;
-        while((mail = filein.readLine()) != null) {
-            if(!mail.contains("@")) continue;
-            mails.add(mail);
-        }
-        
-        for (String m : mails) {
-            query = "INSERT INTO mail (mail) VALUES ('" + m + "');";
-            System.out.println(query);
-            statement.executeUpdate(query);
+            System.out.println(rs.getString(1) + " ==> " + rs.getString(2) + " = " + rs.getDouble(3) + " #" +
+                    rs.getTimestamp(6, Calendar.getInstance()) + " --- " + 
+                    rs.getDate(6,Calendar.getInstance()) + " --- " +
+                    rs.getTime(6, Calendar.getInstance()));
         }
     }
 }
